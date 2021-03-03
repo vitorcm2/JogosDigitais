a72f6cea5ad34b2b49ba10122005be9735697d59
vitorcm2 <vitorcm2@al.insper.edu.br>
2021-03-02 22:59:42 -0300

    Musica, background e bug fixes

---
+++ Breakout/Assets/_Scripts/RepeatBg.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class RepeatBg : MonoBehaviour
+{
+    public float speed = 1.0f;
+
+    //https://www.youtube.com/watch?v=32EIYs6Z18Q&ab_channel=MadFireOn
+    // Update is called olnce per frame
+    void Update()
+    {
+        Vector2 offset = new Vector2(0f, speed) * Time.deltaTime;
+        GetComponent<Renderer>().material.mainTextureOffset += offset;
+    }
+}
